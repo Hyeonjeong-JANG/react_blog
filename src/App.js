d303ae4ce724ec5aa499e891a38bd8c9954644fe
Hyeonjeong_JANG <hyeonjeong.jang.0302@gmail.com>
2023-10-04 11:55:05 +0900

    좋아요 카운트 증가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-// import logo from './logo.svg';
+//
+import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
     let post = '역삼 우동 맛집';
     let [title, b] = useState(['남자 코트 추천', '여자 코트 추천', '사람 코트 추천']);
+    let [like, useLike] = useState(0);
 
     return (
         <div className="App" id={post}>
@@ -12,7 +14,17 @@ function App() {
                 <h4>블로그</h4>
             </div>
             <div className="list">
-                <h4>{title[0]}</h4>
+                <h4>
+                    {title[0]}
+                    <span
+                        onClick={() => {
+                            useLike(like + 1);
+                        }}
+                    >
+                        👍
+                    </span>
+                    {like}
+                </h4>
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
